f424a400a07c79662c63b0ec6c51cc000e21eaab
Asim <asimpsy11@gmail.com>
2023-12-26 22:37:50 +0100

    New cheanges done

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,12 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, Image, TextInput } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+} from "react-native-heroicons/outline";
 
 export default function HomeScreen() {
   const navigation = useNavigation();
@@ -13,8 +19,40 @@ export default function HomeScreen() {
   }, []);
 
   return (
-    <SafeAreaView>
-      <Text className="text-red-500">HomeScreen</Text>
+    // { Header }
+    <SafeAreaView className="bg-white pt-5">
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{ uri: "https://picsum.photos/200/300" }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        ></Image>
+
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color={"#00CCBB"} />
+          </Text>
+        </View>
+
+        <UserIcon size={35} color={"#00CCBB"} />
+      </View>
+
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row space-x-2 p-3 bg-gray-200 flex-1">
+          <MagnifyingGlassIcon color="gray" size={20} />
+          <TextInput
+            placeholder="Resturant & cuisines"
+            keyboardType="default"
+          />
+        </View>
+
+        <AdjustmentsVerticalIcon color={"#00CCBB"} />
+      </View>
+
+      {/* Body */}
+      <View></View>
     </SafeAreaView>
   );
 }
