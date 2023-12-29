9f771044dbfac544758dfb4333a2a8e1b89a85ac
Asim <asimpsy11@gmail.com>
2023-12-29 21:14:54 +0100

    card first item is ready

--- components/ResturantFoodCard.tsx
+++ components/ResturantFoodCard.tsx
@@ -1,5 +1,7 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
+import { StarIcon } from "react-native-heroicons/solid";
+import { MapPinIcon } from "react-native-heroicons/outline";
 
 interface Props {
   id: number;
@@ -34,8 +36,20 @@ const ResturantFoodCard = ({
         }}
         className="h-36 w-64 rounded-sm"
       />
-      <View>
-        <Text className="font-bold text-lg p-2">{title}</Text>
+
+      <View className="px-3 pb-4">
+        <Text className="font-bold text-lg pt-2">{title}</Text>
+        <View className="flex-row items-center space-x-1">
+          <StarIcon color="green" opacity={0.5} size={22} />
+          <Text className="text-xs text-gray-500">
+            <Text className="text-green-500">{rating}</Text> . {genre}
+          </Text>
+        </View>
+
+        <View className="flex-row items-center space-x-1">
+          <MapPinIcon color="gray" size={22} opacity={0.4} />
+          <Text className="text-xs text-gray-500">Nearby . {address}</Text>
+        </View>
       </View>
     </TouchableOpacity>
   );
