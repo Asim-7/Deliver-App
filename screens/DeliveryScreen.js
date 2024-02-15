eee33122fdff678c924e9ad028cea27a22b50770
Asim <asimpsy11@gmail.com>
2024-02-15 21:18:49 +0100

    Card added

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -1,6 +1,12 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import {
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+} from "react-native";
 import { useSelector } from "react-redux";
 import { selectResturant } from "../features/resturantSlice";
 import { XMarkIcon } from "react-native-heroicons/solid";
@@ -17,6 +23,19 @@ function DeliveryScreen() {
             <XMarkIcon color="white" size={30} />
           </TouchableOpacity>
           <Text className="font-light text-white text-lg">Order help</Text>
+        </View>
+
+        <View className="bg-white mx-5 my-2 rounded-md p-6 z-50 shadow-md">
+          <View className="flex-row justify-between">
+            <View>
+              <Text className="text-lg text-gray-400">Estimated time</Text>
+              <Text className="text-4xl font-bold">40-45 mins</Text>
+            </View>
+            <Image
+              source={{ uri: "https://picsum.photos/200/300" }}
+              className="h-20 w-20"
+            />
+          </View>
         </View>
       </SafeAreaView>
     </View>
