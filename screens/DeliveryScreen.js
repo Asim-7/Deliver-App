b7b1116252c62d8d71505fca1c71d3d9b4cbf5fd
Asim <asimpsy11@gmail.com>
2024-02-15 14:42:11 +0100

    Close button implemented

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -1,10 +1,24 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { Text, View } from "react-native";
+import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import { useSelector } from "react-redux";
+import { selectResturant } from "../features/resturantSlice";
+import { XMarkIcon } from "react-native-heroicons/solid";
 
 function DeliveryScreen() {
+  const navigation = useNavigation();
+  const resturant = useSelector(selectResturant);
+
   return (
-    <View>
-      <Text>DeliveryScreen</Text>
+    <View className="bg-[#00CCBB] flex-1">
+      <SafeAreaView>
+        <View>
+          <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+            <XMarkIcon color="white" size={30} />
+          </TouchableOpacity>
+          <Text>Order help</Text>
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
