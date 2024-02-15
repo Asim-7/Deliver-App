149f1297b10ff0d51b97828b46031ce05ded5a9d
Asim <asimpsy11@gmail.com>
2024-02-15 14:07:32 +0100

    Order preparing animation added

--- screens/PreparingOrderScreen.js
+++ screens/PreparingOrderScreen.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { SafeAreaView, Text } from "react-native";
+import * as Animateable from "react-native-animatable";
 
 function PreparingOrderScreen() {
   return (
     <SafeAreaView className="bg-[#00CCBB] flex-1 items-center justify-center">
-      <Text>PreparingOrderScreen</Text>
+      <Animateable.Image
+        source={require("../assets/orderLoading.gif")}
+        animation="slideInUp"
+        iterationCount={1}
+        className="h-96 w-96"
+      />
     </SafeAreaView>
   );
 }
