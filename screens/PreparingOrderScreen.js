1b6c6f57b24209a1231b6d0a8289c7095b42a147
Asim <asimpsy11@gmail.com>
2024-02-15 13:55:15 +0100

    Order screen ui added

---
+++ screens/PreparingOrderScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+function PreparingOrderScreen() {
+  return (
+    <SafeAreaView className="bg-[#00CCBB] flex-1 items-center justify-center">
+      <Text>PreparingOrderScreen</Text>
+    </SafeAreaView>
+  );
+}
+
+export default PreparingOrderScreen;
